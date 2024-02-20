6f2177d08ae6099e0c2167625b9a91114807ad14
Carlos Molina <105681853+Crlziito04@users.noreply.github.com>
2024-02-20 11:12:30 -0300

    [MetodoPut]

--- src/controllers/episodeController.js
+++ src/controllers/episodeController.js
@@ -26,6 +26,27 @@ module.exports = {
       res.status(400).json({ error: error.message });
     }
   },
-  putEpisode: (req, res) => {},
+  putEpisode: async (req, res) => {
+    const { id } = req.params;
+    const stringId = parseInt(id);
+    const { name, episode, completed } = req.body;
+    const idEpisode = await dataBase.allEpisodes.findIndex(
+      (episode) => episode.id === stringId
+    );
+    console.log(stringId);
+    if (idEpisode === -1) {
+      return res.status(404).json({ error: "Episode not found" });
+    }
+    dataBase.allEpisodes[idEpisode] = {
+      id: dataBase.allEpisodes[idEpisode].id,
+      name: name || dataBase.allEpisodes[idEpisode].name,
+      episode: episode || dataBase.allEpisodes[idEpisode].episode,
+      completed: completed || dataBase.allEpisodes[idEpisode].completed,
+    };
+    res.status(200).json({
+      message: "Episodio Actualizado",
+      Episodio: dataBase.allEpisodes,
+    });
+  },
   deleteEpisode: (req, res) => {},
 };
--- src/routes/episodeRouter.js
+++ src/routes/episodeRouter.js
@@ -2,11 +2,15 @@ const { Router } = require("express");
 const {
   getEpisode,
   postEpisode,
+  putEpisode,
+  deleteEpisode,
 } = require("../controllers/episodeController.js");
 
 const episodeRouter = Router();
 
 episodeRouter.get("/", getEpisode);
 episodeRouter.post("/:id", postEpisode);
+episodeRouter.put("/:id", putEpisode);
+episodeRouter.delete("/:id", deleteEpisode);
 
 module.exports = episodeRouter;
